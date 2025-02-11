33d6df4f0c34c92f6b4b482b888a71a645d2bd29
basit011 <156387950+basit011@users.noreply.github.com>
2025-02-11 18:57:11 -0500

    Initial Commit

---
+++ Lab1_Basit_ali/Lab1_Basit_aliApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Basit_aliApp.swift
+//  Lab1_Basit_ali
+//
+//  Created by Basit Ali on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Basit_aliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
